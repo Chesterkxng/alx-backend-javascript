caef8e961f34d05fbbcaf03ebdc211df93164127
Chesterkxng <cgoita00@gmail.com>
2024-04-22 20:28:46 +0000

    spread operator

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,10 +1,3 @@
 export default function concatArrays(array1, array2, string) {
-  let concatArray = [];
-  concatArray = array1.concat(array2);
-  let i = 0;
-  while (i < string.length) {
-    concatArray.push(string[i]);
-    i += 1;
-  }
-  return concatArray;
+  return [...array1, ...array2, ...string];
 }
