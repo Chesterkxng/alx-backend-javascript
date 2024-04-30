959d150dda3338a3d0a79fb96d238261e0afd595
Chesterkxng <cgoita00@gmail.com>
2024-04-30 12:26:29 +0000

    edge cases and eslint fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,13 @@
 export default function cleanSet(set, startString) {
+  if (!(set instanceof Set) && typeof startString !== 'string') {
+    return '';
+  }
   const results = [];
-    set.forEach(item => {
-        if (item.startsWith(startString)) {
-            results.push(item.slice(startString.length));
-        }
-    });
+  set.forEach((item) => {
+    if (item.startsWith(startString)) {
+      results.push(item.slice(startString.length));
+    }
+  });
 
-    return results.join("-");
+  return results.join('-');
 }
