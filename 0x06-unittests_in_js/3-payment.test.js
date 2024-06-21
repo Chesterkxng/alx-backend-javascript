f48a84a9d0ed15a865c687fcf9380309f6dc77d2
Chesterkxng <cgoita00@gmail.com>
2024-06-21 19:10:59 +0000

    2 and 3

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,14 @@
+const sinon = require("sinon");
+const sendPaymentRequestToApi = require('./3-payment');
+const {describe, it} = require("mocha");
+const Utils = require('./utils');
+const expect = require("chai").expect;
+
+describe("make sure the same math is used", function() {
+  it("checking the execution of calculateNumber once", function() {
+    const spy = sinon.spy(Utils, "calculateNumber");
+    sendPaymentRequestToApi(100, 20);
+    expect(spy.calledOnce).to.be.true;
+    spy.restore();
+  });
+});
