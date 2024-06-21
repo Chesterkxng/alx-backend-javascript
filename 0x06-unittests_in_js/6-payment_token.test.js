5767f215ffe1e31d7db08b7e4adadd4a137205bf
Chesterkxng <cgoita00@gmail.com>
2024-06-21 19:23:09 +0000

    6

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,12 @@
+const getPaymentTokenFromAPI = require('./6-payment_token');
+const expect = require("chai").expect;
+
+describe('getPaymentTokenFromAPI', () => {
+  it('should return a successful response object when success is true', () => {
+    getPaymentTokenFromAPI(true)
+      .then((response) => {
+	expect(response).to.deep.equal({ data: 'Successful response from the API' });
+	done();
+      })
+  });
+});
